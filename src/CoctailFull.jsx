1504609ab3a340206ce92d57a36b233f6c9f8618
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-01-13 20:05:50 +0100

    Added CoctailFull component

--- src/CoctailFull.jsx
+++ src/CoctailFull.jsx
@@ -1,12 +1,17 @@
-import "./styles/coctail_fullscreen_prompt.css"
+import "./styles/coctail_fullscreen_prompt.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark, faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
 const CoctailFull = () => {
   return (
     <div class="coctail-full">
       <div class="relative-wrapper">
         <div class="coctail-full__container">
-          <i class="fa-solid fa-circle-xmark coctail-full__close"></i>
-          <i class="fa-solid fa-circle-plus add-favourite"></i>
+          <FontAwesomeIcon
+            icon={faCircleXmark}
+            className="coctail-full__close"
+          />
+          <FontAwesomeIcon icon={faCirclePlus} className="add-favourite" />
           <img alt="#" src={"./img/category.jpg"} />
           <div class="coctail-full__text">
             <h4>Name of Coctail</h4>
@@ -15,7 +20,7 @@ const CoctailFull = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CoctailFull
+export default CoctailFull;
