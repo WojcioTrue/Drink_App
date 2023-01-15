0170e9da9c202eb66b6bce09a16c018a73f29be0
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-01-15 21:00:13 +0100

    Added event chandler for Selecting category

--- src/CategoriesList.jsx
+++ src/CategoriesList.jsx
@@ -1,31 +1,35 @@
 import "./styles/categories_list.css";
 
 const CategoriesList = () => {
+  function changeCategory(e) {
+    console.log(e.currentTarget.getAttribute("value"));
+  }
+
   return (
     <div className="categories-list">
       <h3>Select category:</h3>
       <ul>
-        <li>
+        <li value="whisky" onClick={(e) => changeCategory(e)}>
           <img alt="#" src={"./img/category_icons/whiskey.png"} />
           <p>Whisky</p>
         </li>
-        <li>
+        <li value="Burbon" onClick={(e) => changeCategory(e)}>
           <img alt="#" src="./img/category_icons/bourbon.png" />
           <p>Burbon</p>
         </li>
-        <li>
+        <li value="Vodka" onClick={(e) => changeCategory(e)}>
           <img alt="#" src="./img/category_icons/vodka.png" />
           <p>Vodka</p>
         </li>
-        <li>
+        <li value="Gin" onClick={(e) => changeCategory(e)}>
           <img alt="#" src="./img/category_icons/gin.png" />
           <p>Gin</p>
         </li>
-        <li>
+        <li value="Tequila" onClick={(e) => changeCategory(e)}>
           <img alt="#" src="./img/category_icons/Tequila.png" />
           <p>Tequila</p>
         </li>
-        <li>
+        <li value="Rum" onClick={(e) => changeCategory(e)}>
           <img alt="#" src="./img/category_icons/Rum.png" />
           <p>Rum</p>
         </li>
