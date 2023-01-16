aaedbb919b58c0a16c22a80a35666c55dcf29ca5
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-01-16 18:22:49 +0100

    renaming variables

--- src/FavList.jsx
+++ src/FavList.jsx
@@ -3,13 +3,12 @@ import "./styles/fav_list.css";
 
 const CoctailsFavourite = ({listOfFav}) => {
   const listOfFavourite = listOfFav;
-  console.log(listOfFavourite);
   return (
     <section className="coctails-favourite">
       <h3>Favourite drinks:</h3>
       <ul>
         {listOfFavourite.map(({name, id}) => {
-          return <FavouriteListElement key={id} element={name}/>
+          return <FavouriteListElement key={id} name={name}/>
         })}
       </ul>
     </section>
--- src/FavListElement.jsx
+++ src/FavListElement.jsx
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 
-const FavouriteListElement = ({element}) => {
+const FavouriteListElement = ({name}) => {
   return (
     <li>
-      <h4>{element}</h4>
+      <h4>{name}</h4>
       <FontAwesomeIcon icon={faMinusCircle} className="remove-favourite" />
     </li>
   );
