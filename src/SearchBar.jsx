2d110cca0e969ae25c6883c1d0cb96df108cda61
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-01-25 18:45:55 +0100

    Added async searching by name

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -5,22 +5,25 @@ import {
   faMartiniGlassCitrus,
 } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const Searchbar = () => {
   const [searchDrink, setSearchDrink] = useState("");
+  const [drinkList, setDrinkList] = useState();
 
-  const findByName = async () => {
-    const response = await fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchDrink}`
-    );
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchDrink}`
+      );
 
-    const data = await response.json();
-
-    console.log(data);
-  };
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchData();
+  }, [searchDrink]);
 
   const drinkToSearch = (e) => {
-    findByName();
     setSearchDrink(e.target.value);
   };
 
@@ -32,9 +35,11 @@ const Searchbar = () => {
           className="search-bar__logo"
         />
         FindMyDrink.
+        <ul></ul>
       </h2>
       <span className="search-bar__input">
         <input
+          autoComplete="off"
           type="text"
           value={searchDrink}
           id="SearchDrink"
