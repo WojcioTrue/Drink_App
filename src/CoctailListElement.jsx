37fca5b3a4a4f6249672ece65f601cdb63eb0b5a
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-01-27 00:51:57 +0100

    Change color of button after addTofavourite click

--- src/CoctailListElement.jsx
+++ src/CoctailListElement.jsx
@@ -1,9 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 const CoctailElement = ({ id, name, imgSrc, addToFav }) => {
-  const [elementState, setElementState] = useState(false);
+  const [inFavourite, setInFavourite] = useState(false);
   return (
     <section className="grid-coctails__product">
       <div>
@@ -11,12 +12,24 @@ const CoctailElement = ({ id, name, imgSrc, addToFav }) => {
         <h4>{name}</h4>
         <p>Description of Coctail</p>
       </div>
-
-      <FontAwesomeIcon
-        icon={faPlusCircle}
-        className="add-favourite"
-        onClick={() => addToFav({ name: name, id: id })}
-      />
+      {inFavourite ? (
+        <FontAwesomeIcon
+          icon={faMinusCircle}
+          className="add-favourite remove-color"
+          onClick={() => setInFavourite((prev) => !prev)}
+        />
+      ) : (
+        <FontAwesomeIcon
+          icon={faPlusCircle}
+          className="add-favourite"
+          onClick={() =>
+            addToFav(
+              { name: name, id: id },
+              setInFavourite((prev) => !prev)
+            )
+          }
+        />
+      )}
     </section>
   );
 };
