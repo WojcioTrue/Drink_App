59515451a207e61c8b7c5cb2664208efafdb3e1e
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-03-01 19:46:39 +0100

    Styled information when there is no favouriteListElements

--- src/FavListFullScreen.jsx
+++ src/FavListFullScreen.jsx
@@ -2,40 +2,43 @@ import { useContext } from "react";
 import "./styles/coctail_list.css";
 import { MyContext } from "./context/ContextComponent";
 import CoctailListElement from "./CoctailListElement";
-import "./styles/fav_list_fullscreen.css"
+import "./styles/fav_list_fullscreen.css";
 
 const FavListFullScreen = () => {
   const { listOfFav } = useContext(MyContext);
   const favouriteList = listOfFav.drinks;
 
-
   return (
     <div className="list-coctails">
-      
-        {favouriteList.length > 0 ? (
-          <>
+      {favouriteList.length > 0 ? (
+        <>
           <h3>FavouriteDrinks:</h3>
           <div className="grid-coctails">
             {favouriteList.map((element) => {
-            return (
-              <CoctailListElement
-                key={element.idDrink}
-                id={element.idDrink}
-                name={element.strDrink}
-                imgSrc={element.strDrinkThumb}
-              />
-            );
-          })}
+              return (
+                <CoctailListElement
+                  key={element.idDrink}
+                  id={element.idDrink}
+                  name={element.strDrink}
+                  imgSrc={element.strDrinkThumb}
+                />
+              );
+            })}
           </div>
-          </>
-        ) : (
-          <div className="empty-list">
+        </>
+      ) : (
+        <div className="empty-list">
+          <span className="empty-list-img">
             <img alt="#" src="./img/fav_icon.png"></img>
+          </span>
+          <section>
             <h3>Your list is empty!</h3>
-            <p>Add something to your favourite list. drink responsibly!</p>
-          </div>
-        )}
-      </div>
+            <p>Add something to your favourite list.</p>
+            <p>Drink responsibly!</p>
+          </section>
+        </div>
+      )}
+    </div>
   );
 };
 
