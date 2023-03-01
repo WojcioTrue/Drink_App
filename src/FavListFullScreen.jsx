3738127eb1ae546d55c0e943e36fe235b26260dd
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-03-01 01:07:04 +0100

    imported styles and maped through favouriteList

--- src/FavListFullScreen.jsx
+++ src/FavListFullScreen.jsx
@@ -1,9 +1,28 @@
-
+import { useContext } from "react";
+import "./styles/coctail_list.css";
+import { MyContext } from "./context/ContextComponent";
+import CoctailListElement from "./CoctailListElement";
 
 const FavListFullScreen = () => {
+  const { listOfFav } = useContext(MyContext);
+  const favouriteList = listOfFav.drinks;
   return (
-    <div>FavListFullScreen</div>
-  )
-}
+    <div className="list-coctails">
+      <h3>List of coctails:</h3>
+      <div className="grid-coctails">
+        {favouriteList.map((element) => {
+          return (
+            <CoctailListElement
+              key={element.idDrink}
+              id={element.idDrink}
+              name={element.strDrink}
+              imgSrc={element.strDrinkThumb}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default FavListFullScreen
+export default FavListFullScreen;
