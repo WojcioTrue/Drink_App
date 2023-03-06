ff4833b6074da4e78993ea4afa59baa789623ff6
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-03-06 13:54:50 +0100

    Removed useContext from App

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import Searchbar from "./SearchBar";
 import CoctailsContainer from "./CoctailsContainer";
 import FavButton from "./FavButton";
 import NotificationList from "./NotificationList";
-import { useContext } from "react";
-import { MyContext } from "./context/ContextComponent";
 
 function App() {
-
-  const { alertList } = useContext(MyContext);
-  console.log(alertList);
-
   return (
     <div className="main-container">
       <Searchbar />
       <CoctailsContainer />
       <FavButton />
-      <NotificationList/>
+      <NotificationList />
     </div>
   );
 }
--- src/NotificationList.jsx
+++ src/NotificationList.jsx
@@ -1,9 +1,14 @@
 import NotificationPrompt from "./NotificationPrompt";
+import { useContext } from "react";
+import { MyContext } from "./context/ContextComponent";
+import "./styles/notification_prompt.css";
 
 const NotificationList = () => {
+  const { alertList } = useContext(MyContext);
+  console.log(alertList);
   return (
-  <div className="favourite-notification-list">
-    NotificationList
+    <div className="favourite-notification-list">
+        <NotificationPrompt/>
     </div>
     )
 };
