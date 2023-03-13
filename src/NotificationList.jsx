d6d0c19de5203cd2713bc3c856f428516178b6da
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-03-13 17:06:37 +0100

    Removed useless Hooks

--- src/NotificationList.jsx
+++ src/NotificationList.jsx
@@ -1,4 +1,4 @@
-import NotificationPrompt from "./NotificationPrompt";
+import NotificationPromptCheck from "./NotificationPromptCheck";
 import { useContext } from "react";
 import { MyContext } from "./context/ContextComponent";
 import "./styles/notification_prompt.css";
@@ -6,12 +6,12 @@ import "./styles/notification_prompt.css";
 const NotificationList = () => {
   const { alertList } = useContext(MyContext);
 
-  
+  console.log(alertList);
 
   return (
     <div className="favourite-notification-list">
         {alertList.length > 0 && alertList.map((element, index) => 
-          <NotificationPrompt key={index} added={element} />
+          <NotificationPromptCheck key={index} added={element.isAdded} />
         )}
     </div>
     )
