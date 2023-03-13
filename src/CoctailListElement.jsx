13c8467e718e39d235c269d7fb73041c20bd1990
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-03-13 21:42:03 +0100

    Added whileInView option for drinks in grid

--- src/CoctailListElement.jsx
+++ src/CoctailListElement.jsx
@@ -1,20 +1,30 @@
 import { Link } from "react-router-dom";
 import Button from "./sharedComponents/Button";
 import AddRemButton from "./sharedComponents/AddRemButton";
+import { motion } from "framer-motion";
+import { singleDrink } from "./framerStyles/variants";
 
 const CoctailElement = ({ id, name, imgSrc }) => {
-  
+  console.log(singleDrink);
+
   return (
-    <section className="grid-coctails__product">
+    <motion.section
+      // for some reason variants dont work with whileInView
+      // option...
+      initial={{ opacity: 0.2, x: 15 }}
+      whileInView={{ opacity: 1, x: 0, transition: { duration: 0.3 } }}
+      viewport={{ once: true, margin: "-100px" }}
+      className="grid-coctails__product"
+    >
       <div>
         <img alt="#" src={imgSrc} />
         <h4>{name}</h4>
       </div>
       <Link to={`/${id}`}>
-        <Button variant="product-button" >{"Details"}</Button>
+        <Button variant="product-button">{"Details"}</Button>
       </Link>
-      <AddRemButton name={name} id={id} img={imgSrc} className="grid-button"/>
-    </section>
+      <AddRemButton name={name} id={id} img={imgSrc} className="grid-button" />
+    </motion.section>
   );
 };
 
--- src/framerStyles/variants.jsx
+++ src/framerStyles/variants.jsx
@@ -16,12 +16,18 @@ export const categoryGestures = {
 
 export const drinkInfo = {
   hidden: { y: -10, opacity: 0 },
-  show: (i) =>  ({
+  show: (i) => ({
     y: 0,
     opacity: 1,
     transition: {
-      duration: 0.2,
+      duration: 0.25,
       delay: i * 0.2,
     },
   }),
+};
+
+export const singleDrink = {
+  hidden: { opacity: 0, x: 15},
+  show: { opacity: 1, x : 0, transition: { duration: 2 } },
+  whileInView : { opacity: 1, x: 0, transition: { duration: 0.3 } }
 };
