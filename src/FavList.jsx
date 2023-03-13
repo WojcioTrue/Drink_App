4588d6cc69f28ab3772fbd525d1c96851da5cfcd
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-03-13 18:35:53 +0100

    Added motion to favouriteList

--- src/FavList.jsx
+++ src/FavList.jsx
@@ -4,6 +4,8 @@ import { useContext } from "react";
 import { MyContext } from "./context/ContextComponent";
 import { Link } from "react-router-dom";
 import Button from "./sharedComponents/Button";
+import { motion } from "framer-motion";
+import { mainView } from "./framerStyles/variants";
 
 const CoctailsFavourite = () => {
   const { listOfFav } = useContext(MyContext);
@@ -11,7 +13,9 @@ const CoctailsFavourite = () => {
   const listOfFavourite = listOfFav.drinks;
   let afterSlice;
   const returnFavourite = ({ strDrink, idDrink }) => {
-    return <FavouriteListElement key={idDrink} strDrink={strDrink} id={idDrink} />;
+    return (
+      <FavouriteListElement key={idDrink} strDrink={strDrink} id={idDrink} />
+    );
   };
 
   const sliceFavourite = () => {
@@ -22,16 +26,28 @@ const CoctailsFavourite = () => {
     }
   };
   sliceFavourite();
-  
+
   return (
-    <section className="coctails-favourite">
+    <motion.section
+      variants={mainView}
+      custom={0.5}
+      initial="hidden"
+      animate="show"
+      className="coctails-favourite"
+    >
       <h3>
         {"Favourite drinks "}
         {listOfFavourite.length > 0 && `(${listOfFavourite.length})`}:
       </h3>
       <ul>{afterSlice}</ul>
-      {listOfFavourite.length > 0 && <Link to="/favourite_list"><Button variant="coctails-favourite__button">{"View all favourite drinks"}</Button></Link>}
-    </section>
+      {listOfFavourite.length > 0 && (
+        <Link to="/favourite_list">
+          <Button variant="coctails-favourite__button">
+            {"View all favourite drinks"}
+          </Button>
+        </Link>
+      )}
+    </motion.section>
   );
 };
 
