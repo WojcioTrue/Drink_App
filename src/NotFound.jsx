54ef4b5de25b392911922a8405d2a320c7dad9d8
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-03-15 20:22:24 +0100

    Added initial animation for NotFound component

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -3,11 +3,13 @@ import Button from "./sharedComponents/Button";
 import { Link } from "react-router-dom";
 import './styles/not_found.css'
 import { coctailButton } from './framerStyles/variants'
+import { noFavouriteDrinks } from './framerStyles/variants'
 
 const NotFound = () => {
   return (
     <div className="not-found">
       <Message
+        animationVariant={noFavouriteDrinks}
         header={"Something went wrong!"}
         text={"I don't see the page you looking for, or some error occured..."}
         secondText={"Return to home page."}
