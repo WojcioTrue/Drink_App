083335154f6bfcee4a20bff2fea283395f396e1b
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-03-28 13:14:29 +0200

    Moved route to separate file and created Routes folder

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,28 @@
+import DrinkInfo from "../components/coctailListComponents/DrinkInfo";
+import NotFound from "../sharedComponents/NotFound";
+import CoctailList from "../components/coctailListComponents/CoctailList";
+import FavListFullScreen from "../components/favouriteListComponents/FavListFullScreen";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const RouteComponent = () => {
+  return (
+    <Routes>
+        {/* home/index route */}
+        <Route path="/" element={<CoctailList />} />
+        {/* path for drink with id */}
+        <Route path="/drink/:id" element={<DrinkInfo />} />
+        {/* categories */}
+        <Route path="categories">
+          <Route path=":id" element={<CoctailList/>}/>
+        </Route>
+        {/* favourite list route */}
+        <Route path="/favourite_list" element={<FavListFullScreen />} />
+        {/* not found element */}
+        <Route path="/error" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/error" />} />
+        
+      </Routes>
+  )
+}
+
+export default RouteComponent
--- src/components/categoryComponents/CategoryList.jsx
+++ src/components/categoryComponents/CategoryList.jsx
@@ -1,11 +1,8 @@
 import "../../styles/categories_list.css";
 import "./CategoryListElement";
 import CategoryListElement from "./CategoryListElement";
-import DrinkInfo from "../coctailListComponents/DrinkInfo";
-import NotFound from "../../sharedComponents/NotFound";
-import CoctailList from "../coctailListComponents/CoctailList";
-import FavListFullScreen from "../favouriteListComponents/FavListFullScreen";
-import { Routes, Route, Link, Navigate } from "react-router-dom";
+import RouteComponent from "../../Routes/Routes";
+import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import { mainView } from "../../framerStyles/variants";
 import { arrayOfCategories } from "./CategoriesArray";
@@ -33,22 +30,7 @@ const CategoryList = () => {
         </ul>
       </motion.div>
 
-      <Routes>
-        {/* home/index route */}
-        <Route path="/" element={<CoctailList />} />
-        {/* path for drink with id */}
-        <Route path="/drink/:id" element={<DrinkInfo />} />
-        {/* categories */}
-        <Route path="categories">
-          <Route path=":id" element={<CoctailList/>}/>
-        </Route>
-        {/* favourite list route */}
-        <Route path="/favourite_list" element={<FavListFullScreen />} />
-        {/* not found element */}
-        <Route path="/error" element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/error" />} />
-        
-      </Routes>
+      <RouteComponent/>
     </>
   );
 };
