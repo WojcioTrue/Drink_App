3262d1d744dd8e6985f5961dbe1170394170fc42
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-04-06 20:01:08 +0200

    removed useless commit

--- src/features/notificationList/NotificationList.jsx
+++ src/features/notificationList/NotificationList.jsx
@@ -5,7 +5,6 @@ import "../../styles/notification_prompt.css";
 const NotificationList = () => {
     const notificationList = useSelector(state => state.notificationList)
 
-    console.log(notificationList);
     return (
       <div className="favourite-notification-list">
           {notificationList.length > 0 && notificationList.map((element) => 
