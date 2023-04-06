508123bc0fedd645aa70a0dfb8aab6335b25f334
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-04-06 19:57:58 +0200

    Removed useless components from components folder

--- src/context/ContextComponent.jsx
+++ src/context/ContextComponent.jsx
@@ -15,9 +15,6 @@ export const MyContext = createContext();
 
 const ContextComponent = ({ children }) => {
   const [drinkData, setDrinkData] = useState(false);
-  // const [listOfFav, setListOfFav] = useState(checkLocal);
-  const [alertList, setAlertList] = useState([]);
-  // false as default value, thanks to that we have 
   // error in API request and we dont display nothing
   const [categoryId, setCategoryId] = useState(false);
   // fetch data with category variables (default "Vodka")
@@ -46,40 +43,12 @@ const ContextComponent = ({ children }) => {
   }, [categoryId]);
 
 
-  //local storage for favourite list
-  // useEffect(() => {
-  //   localStorage.setItem("fav-list", JSON.stringify(listOfFav));
-  // }, [listOfFav]);
-
-  // display prompt and add drink to favourite
-  // const addToFav = (argument) => {
-  //   setAlertList([...alertList, { id: Date.now(), isAdded: true }]);
-  //   const elementExist = listOfFav.drinks.some(
-  //     (element) => element.idDrink === argument.idDrink
-  //   );
-  //   if (elementExist) {
-  //     console.log("element się powtarza");
-  //   } else {
-  //     setListOfFav({ drinks: [argument, ...listOfFav.drinks] });
-  //   }
-  // };
-  // remove element with the same id using filter method
-  // const removeFav = (id) => {
-  //   setAlertList([...alertList, { id: Date.now(), isAdded: false }]);
-  //   const filteredList = listOfFav.drinks.filter(
-  //     (element) => element.idDrink !== id
-  //   );
-  //   setListOfFav({ drinks: filteredList });
-  // };
-
   return (
     <MyContext.Provider
       value={{
         setDrinkData,
-        setAlertList,
         setCategoryId,
         drinkData,
-        alertList,
       }}
     >
       {children}
