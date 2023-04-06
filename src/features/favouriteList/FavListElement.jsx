cc573955385c3247449fe5823d7472e703113783
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-04-06 19:43:43 +0200

    Added addNotification inside FavListElement

--- src/features/favouriteList/FavListElement.jsx
+++ src/features/favouriteList/FavListElement.jsx
@@ -1,28 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 import { useDispatch } from "react-redux";
-import { removeFromFavourite } from "./favouriteListSlice"
+import { removeFromFavourite } from "./favouriteListSlice";
+import { addNotification } from "../notificationList/notificationListSlice";
+
 const FavouriteListElement = ({ strDrink, id }) => {
-
-
   const dispatch = useDispatch();
 
   return (
-    <motion.li
-      initial={{opacity: 0, x : -10}}
-      animate={{opacity: 1, x : 0}}
-    >
+    <motion.li initial={{ opacity: 0, x: -10 }} animate={{ opacity: 1, x: 0 }}>
       <Link to={`/drink/${id}`}>
         <h4>{strDrink}</h4>
       </Link>
 
       <FontAwesomeIcon
         onClick={() => {
-          dispatch(removeFromFavourite({idDrink: id}));
-          }
-        }
+          dispatch(removeFromFavourite({ idDrink: id }));
+          dispatch(addNotification({ id: id, isAdded: false }));
+        }}
         icon={faMinusCircle}
         className="remove-favourite"
       />
