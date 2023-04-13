1a739420cdb0bb3875c660c6601b2a7af23109d9
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-04-13 21:25:17 +0200

    Added extraReducers for thunk async function

--- src/features/categoryList/categoryListSlice.jsx
+++ src/features/categoryList/categoryListSlice.jsx
@@ -1,25 +1,45 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-export const getDrinks = createAsyncThunk(`categoryList/getDrinks`, async () => {
-    const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka`)
-    return response.data
-})
-
-
+export const getDrinks = createAsyncThunk(
+  `categoryList/getDrinks`,
+  async () => {
+    const response = await axios.get(
+      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Vodka`
+    );
+    return response.data;
+  }
+);
 
 const initialState = {
-    category: "Vodka",
-    data: [],
-    error: null
-}
+  data: [],
+  loading: "idle",
+  error: null,
+};
 
 const categoryListSlice = createSlice({
-    name: 'categoryList',
-    initialState,
-    reducers: {
+  name: "categoryList",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getDrinks.pending, (state, action) => {
+      if (state.loading === "idle") {
+        state.loading = "pending";
+      }
+    });
+    builder.addCase(getDrinks.fulfilled, (state, action) => {
+      if (state.loading === "pending") {
+        state.data = action.payload;
+        state.loading = "idle";
+      }
+    });
+    builder.addCase(getDrinks.rejected, (state, action) => {
+      if (state.loading === "pending") {
+        state.loading = "idle";
+        state.error = "Error occured";
+      }
+    });
+  },
+});
 
-    }
-})
-
-export default categoryListSlice.reducer
+export default categoryListSlice.reducer;
