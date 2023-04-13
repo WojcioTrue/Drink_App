69dbc3e74ed33fe413bfc54437e2ef03eb102742
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-04-13 20:39:31 +0200

    Added categoryList to store

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,12 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import favouriteListReducer from "../features/favouriteList/favouriteListSlice";
 import notificationListReducer from "../features/notificationList/notificationListSlice"
-
+import categoryListReducer from "../features/categoryList/categoryListSlice";
 
 
 export default configureStore({
     reducer: {
         favouriteList: favouriteListReducer,
-        notificationList: notificationListReducer
+        notificationList: notificationListReducer,
+        categoryList: categoryListReducer
     }
 })
