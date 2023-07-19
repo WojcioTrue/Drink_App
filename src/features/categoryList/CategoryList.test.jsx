8a1a470ea32c855d4288a6f27b919c7c044e6b82
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-07-19 17:16:00 +0200

    written tests for CategoryListElement and CategoryList

--- src/features/categoryList/CategoryList.test.jsx
+++ src/features/categoryList/CategoryList.test.jsx
@@ -1,12 +1,12 @@
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
 import CategoryList from "./CategoryList";
 import { MemoryRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "../../app/store";
 
-describe("basic tests", () => {
-  test("display header form category lista", () => {
+describe("Tests for category list elements", () => {
+  test("display header form category list", () => {
     render(
       <MemoryRouter>
         <Provider store={store}>
@@ -18,5 +18,18 @@ describe("basic tests", () => {
       name: /Select category:/i,
     });
     expect(categoryListHeader).toBeInTheDocument();
+  });
+  test("check elements of list in right order", () => {
+    render(
+      <MemoryRouter>
+        <Provider store={store}>
+          <CategoryList />
+        </Provider>
+      </MemoryRouter>
+    );
+
+    const categoriesList = screen.getAllByRole("listitem");
+    categoriesList.forEach(listElement =>
+      expect(listElement).toBeInTheDocument())
   });
 });
---
+++ src/features/categoryList/CategoryListElement.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import CategoryListElement from "./CategoryListElement";
+import {arrayOfCategories} from "./CategoriesArray"
+
+
+describe("Loop every element from CategoryList", () => {
+    arrayOfCategories.forEach(element => {
+        test(`check ${element.text} element of list`, () => {
+        render(
+        <CategoryListElement 
+        imgSrc={element.img} 
+        text={element.text}
+        />)
+        const singleElement = screen.getByText(element.text)
+        expect(singleElement).toBeInTheDocument()
+        console.log(element.img)
+        const singleElementImg = screen.getByRole('img')
+        expect(singleElementImg.getAttribute('src')).toBe(element.img)
+    })})
+})
