8e4089acd28d86ca1ddec05c697e0936f7474f92
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-08-31 18:22:34 +0200

    created request mocking server

--- src/mocks/handlers.jsx
+++ src/mocks/handlers.jsx
@@ -1,1 +1,12 @@
-import { rest } from 'msw'
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("https://jsonplaceholder.typicode.com/posts", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({
+        name: "just a test",
+      })
+    );
+  }),
+];
