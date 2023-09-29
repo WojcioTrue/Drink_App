9333fd8ea215bbe47d0950bc8a33265eac6d504f
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-09-29 13:55:12 +0200

    finished test for CoctailList

---
+++ src/components/coctailListComponents/testData.jsx
@@ -0,0 +1,137 @@
+export const whiskeyData = {
+  drinks: [
+    {
+      strDrink: "Damned if you do",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/ql7bmx1503565106.jpg",
+      idDrink: "13194",
+    },
+    {
+      strDrink: "Hot Toddy",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/ggx0lv1613942306.jpg",
+      idDrink: "178345",
+    },
+    {
+      strDrink: "Owen's Grandmother's Revenge",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/0wt4uo1503565321.jpg",
+      idDrink: "13200",
+    },
+  ],
+};
+
+export const vodkaData = {
+  drinks: [
+    {
+      strDrink: "155 Belmont",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/yqvvqs1475667388.jpg",
+      idDrink: "15346",
+    },
+    {
+      strDrink: "501 Blue",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/ywxwqs1461867097.jpg",
+      idDrink: "17105",
+    },
+    {
+      strDrink: "57 Chevy with a White License Plate",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/qyyvtu1468878544.jpg",
+      idDrink: "14029",
+    },
+  ],
+};
+
+export const tequilaData = {
+  drinks: [
+    {
+      strDrink: "110 in the shade",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/xxyywq1454511117.jpg",
+      idDrink: "15423",
+    },
+    {
+      strDrink: "3-Mile Long Island Iced Tea",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg",
+      idDrink: "15300",
+    },
+    {
+      strDrink: "Adam Bomb",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/tpxurs1454513016.jpg",
+      idDrink: "16333",
+    },
+  ],
+};
+
+export const rumData = {
+  drinks: [
+    {
+      strDrink: "Adam Bomb",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/tpxurs1454513016.jpg",
+      idDrink: "16333",
+    },
+    {
+      strDrink: "Adios Amigos Cocktail",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/8nk2mp1504819893.jpg",
+      idDrink: "17229",
+    },
+    {
+      strDrink: "Aztec Punch",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/uqwuyp1454514591.jpg",
+      idDrink: "14364",
+    },
+  ],
+};
+
+export const ginData = {
+  drinks: [
+    {
+      strDrink: "3-Mile Long Island Iced Tea",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg",
+      idDrink: "15300",
+    },
+    {
+      strDrink: "69 Special",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/vqyxqx1472669095.jpg",
+      idDrink: "13940",
+    },
+    {
+      strDrink: "A1",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg",
+      idDrink: "17222",
+    },
+  ],
+};
+
+export const bourbonData = {
+  drinks: [
+    {
+      strDrink: "Allegheny",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/uwvyts1483387934.jpg",
+      idDrink: "11021",
+    },
+    {
+      strDrink: "Bourbon Sour",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/dms3io1504366318.jpg",
+      idDrink: "11147",
+    },
+    {
+      strDrink: "Brandon and Will's Coke Float",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/xspxyr1472719185.jpg",
+      idDrink: "16447",
+    },
+  ],
+};
--- src/features/categoryList/CategoryList.test.jsx
+++ src/features/categoryList/CategoryList.test.jsx
@@ -1,35 +1,22 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import CategoryList from "./CategoryList";
-import { MemoryRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "../../app/store";
+import { renderWithProviders } from "../../utils/test-utils";
 
 describe("Tests for category list elements", () => {
   test("display header form category list", () => {
-    render(
-      <MemoryRouter>
-        <Provider store={store}>
-          <CategoryList />
-        </Provider>
-      </MemoryRouter>
-    );
+    renderWithProviders(<CategoryList />);
     const categoryListHeader = screen.getByRole("heading", {
       name: /Select category:/i,
     });
     expect(categoryListHeader).toBeInTheDocument();
   });
   test("check elements of list in right order", () => {
-    render(
-      <MemoryRouter>
-        <Provider store={store}>
-          <CategoryList />
-        </Provider>
-      </MemoryRouter>
-    );
+    renderWithProviders(<CategoryList />);
 
     const categoriesList = screen.getAllByRole("listitem");
-    categoriesList.forEach(listElement =>
-      expect(listElement).toBeInTheDocument())
+    categoriesList.forEach((listElement) =>
+      expect(listElement).toBeInTheDocument()
+    );
   });
 });
