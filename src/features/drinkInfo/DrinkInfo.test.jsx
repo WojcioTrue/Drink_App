0f9fede66b6cedc18e0430d38a9ac1e64cbc5989
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-10-04 13:25:57 +0200

    Added mocking for test data inside DrinkInfo

--- src/features/drinkInfo/DrinkInfo.test.jsx
+++ src/features/drinkInfo/DrinkInfo.test.jsx
@@ -1,34 +1,38 @@
-import {
-  render,
-  screen,
-  waitForElementToBeRemoved,
-} from "@testing-library/react";
+import { screen, waitForElementToBeRemoved } from "@testing-library/react";
 import DrinkInfo from "./DrinkInfo";
 import "react-intersection-observer/test-utils";
-import { MemoryRouter, useParams} from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "../../app/store";
+import { setupServer } from "msw/lib/node";
+import { rest } from "msw";
+import { testDrink } from "./drinkInfoTestData";
+import { renderWithProviders } from "../../utils/test-utils";
+import { setupStore } from "../../app/store";
+import { getDrinkData } from "./drinkDataSlice";
 
-jest.mock('react-router-dom', () => ({
-    ...jest.requireActual('react-router-dom'),
-    useParams: () => ({
-        id: "17105",
-    })
-}))
+const server = setupServer(
+  rest.get(
+    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php`,
+    (req, res, ctx) => {
+      const drinkId = req.url.searchParams.get("i");
+      if (drinkId === "17105") return res(ctx.status(404), ctx.json(testDrink));
+    }
+  )
+);
 
-const customRoute = "/drink/17105";
+beforeAll(() => server.listen({ onUnhandledRequest: "bypass" }));
+afterAll(() => server.close());
+
 describe("test for single drink with provided route parameter", () => {
   test("display component", async () => {
-    render(
-      <MemoryRouter initialEntries={[customRoute]}>
-        <Provider store={store}>
-          <DrinkInfo />
-        </Provider>
-      </MemoryRouter>
-    );
+    // setup initial store for redux wrapper
+    const store = setupStore();
+    // dispatching drinkId for mocked data
+    store.dispatch(getDrinkData("17105"));
+    renderWithProviders(<DrinkInfo />, { store });
 
     await waitForElementToBeRemoved(await screen.findByText(/Loading.../i));
 
-    screen.debug()
+    const drinkName = screen.getByRole("heading", { name: /501 blue/i });
+
+    expect(drinkName).toBeInTheDocument();
   });
 });
