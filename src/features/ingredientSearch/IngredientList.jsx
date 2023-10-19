050a9a0ddd641bba565d4123f7c607a6b7d9a2a1
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-10-19 18:03:47 +0200

    first concept for ingredient list

--- src/features/ingredientSearch/IngredientList.jsx
+++ src/features/ingredientSearch/IngredientList.jsx
@@ -1,9 +1,37 @@
-
+import { nanoid } from "@reduxjs/toolkit";
+import { useState, useEffect } from "react";
 
 const IngredientList = () => {
-  return (
-    <div>IngredientList</div>
-  )
-}
+  const [ingredientList, setIngredientList] = useState([]);
+    const [onList, setOnList] = useState([])
 
-export default IngredientList
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      try {
+        const response = await fetch(
+          "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setIngredientList(data.drinks);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchIngredients();
+  }, []);
+
+  return (
+    <ul>
+      {ingredientList.slice(0,7).map((element, index) => (
+        <li key={nanoid()}>
+            <label>Ingredient {index} </label> 
+            {element.strIngredient1}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default IngredientList;
