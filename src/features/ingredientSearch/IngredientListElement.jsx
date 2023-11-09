5ab14b3097d5ea03ac49114ebed389f6f795ca44
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-11-09 13:09:53 +0100

    Styled ingredient selector

--- src/features/ingredientSearch/IngredientListElement.jsx
+++ src/features/ingredientSearch/IngredientListElement.jsx
@@ -1,4 +1,4 @@
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { nanoid } from "@reduxjs/toolkit";
 import { useEffect, useState } from "react";
@@ -11,47 +11,50 @@ const IngredientListElement = ({
   changeSelected,
   onList,
 }) => {
-  const [removedDuplicates, setRemovedDuplicates] = useState([])
+  const [removedDuplicates, setRemovedDuplicates] = useState([]);
   const id = listElement.id;
 
   useEffect(() => {
-    let shallowCopy = [...ingredientList]
-    for(let element of onList){
+    let shallowCopy = [...ingredientList];
+    for (let element of onList) {
       // make selected element visible on list
-      if(element.value === listElement.value){
-        continue
+      if (element.value === listElement.value) {
+        continue;
       } else {
         // remove duplicate of ingredient
-        const index = shallowCopy.indexOf(element.value)
-        shallowCopy.splice(index, 1)
+        const index = shallowCopy.indexOf(element.value);
+        shallowCopy.splice(index, 1);
       }
     }
-    setRemovedDuplicates(shallowCopy)
-  },[onList,ingredientList, listElement])
+    setRemovedDuplicates(shallowCopy);
+  }, [onList, ingredientList, listElement]);
 
   return (
     <li>
       <label>Ingredient {listNumber + 1} : </label>
-      <select
-        onChange={() => changeSelected(id, document.getElementById(id).value)}
-        name={id}
-        id={id}
-        value={listElement.value}
-      >
-        <option value="">--  Please choose an option  --</option>
-        {removedDuplicates.map((element) => (
-          <option key={nanoid()} value={element}>
-            {element}
-          </option>
-        ))}
-      </select>
-      {/* <FontAwesomeIcon
-        className="remove-ingredient"
-        onClick={() => {
-          removeIngredient(id);
-        }}
-        icon={faXmark}
-      /> */}
+      <div>
+        <select
+          onChange={() => changeSelected(id, document.getElementById(id).value)}
+          name={id}
+          id={id}
+          value={listElement.value}
+        >
+          <option value="">-- Please choose an option --</option>
+          {removedDuplicates.map((element) => (
+            <option key={nanoid()} value={element}>
+              {element}
+            </option>
+          ))}
+        </select>
+        <div
+          className="remove-ingredient"
+          onClick={() => {
+            removeIngredient(id);
+          }}
+        >
+          <FontAwesomeIcon className="remove-ingredient--icon" icon={faTrash} />
+        </div>
+      </div>
     </li>
   );
 };
