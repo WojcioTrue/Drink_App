19c68ad15e63d15859c982974d290039c7d84ec0
Wojciech.Otawski <wojciech.otawski@gmail.com>
2023-11-15 14:41:12 +0100

    Added not found condition if there is no category

--- src/components/coctailListComponents/CoctailsOutlet.jsx
+++ src/components/coctailListComponents/CoctailsOutlet.jsx
@@ -1,19 +1,25 @@
 import { motion } from "framer-motion";
 import { mainView } from "../../framerStyles/variants";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import NotFound from "../../sharedComponents/NotFound";
 
 const CoctailsOutlet = () => {
+  const location = useLocation();
+
   return (
-    <motion.div
-      variants={mainView}
-      custom={0.4}
-      initial="hidden"
-      animate="show"
-      className="list-coctails"
-    >
-      <h3>List of coctails:</h3>
-      <Outlet />
-    </motion.div>
+    <>
+      <motion.div
+        variants={mainView}
+        custom={0.4}
+        initial="hidden"
+        animate="show"
+        className="list-coctails"
+      >
+        <h3>List of coctails:</h3>
+        {location.pathname === "/categories/" ? <NotFound /> : <Outlet />}
+      </motion.div>
+    </>
   );
 };
 
