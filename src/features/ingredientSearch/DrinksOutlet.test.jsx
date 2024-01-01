78ab2856c081d0a232d79c4e99a1b750f0da202f
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-01-01 21:14:29 +0100

    Fixed test for DrinksOutlet.test.jsx

--- src/features/ingredientSearch/DrinksOutlet.test.jsx
+++ src/features/ingredientSearch/DrinksOutlet.test.jsx
@@ -1,15 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import "@testing-library/jest-dom";
 import DrinksOutlet from "./DrinksOutlet";
 import { renderWithProviders } from "../../utils/test-utils";
-import { screen } from "@testing-library/react";
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useLocation: jest.fn(),
+}));
 
 describe("Tests for DrinksOutlet", () => {
-  test("Should return 'Drinks by ingredients' heading", () => {
+  test("Should return 'Something went wrong!' page", () => {
+    require("react-router-dom").useLocation.mockReturnValue({
+      pathname: "/ingredients/",
+    });
     renderWithProviders(<DrinksOutlet />);
 
-    const drinkHeading = screen.getByRole("heading", {
-      name: /Drinks by ingredients/i,
-    });
+    const errorHeader = screen.getByText(/something went wrong!/i);
+    expect(errorHeader).toBeInTheDocument();
+    const firstErrorMessage = screen.getByText(
+      /i don't see the page you looking for, or some error occured.../i
+    );
+    expect(firstErrorMessage).toBeInTheDocument();
+    const secondErrorMessage = screen.getByText(/return to home page./i);
+    expect(secondErrorMessage).toBeInTheDocument();
 
-    expect(drinkHeading).toBeInTheDocument();
+    const returnButton = screen.getByRole("button", { name: /return home/i });
+    expect(returnButton).toBeInTheDocument();
   });
 });
---
+++ src/features/ingredientSearch/ginData.jsx
@@ -0,0 +1,28 @@
+export const DrinksOutletData = {
+  drinks: [
+    {
+      strDrink: "3-Mile Long Island Iced Tea",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg",
+      idDrink: "15300",
+    },
+    {
+      strDrink: "Long Island Iced Tea",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/wx7hsg1504370510.jpg",
+      idDrink: "17204",
+    },
+    {
+      strDrink: "Long Island Tea",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/nkwr4c1606770558.jpg",
+      idDrink: "11002",
+    },
+    {
+      strDrink: "The Strange Weaver",
+      strDrinkThumb:
+        "https://www.thecocktaildb.com/images/media/drink/opxjzh1604179528.jpg",
+      idDrink: "178339",
+    },
+  ],
+};
