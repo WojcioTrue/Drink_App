8b67c8543704d91a95de5a8c808edae3c52fed2e
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-01-01 18:30:20 +0100

    Removed useless mocking from DrinksOutlet.test.jsx

--- src/features/ingredientSearch/DrinksOutlet.test.jsx
+++ src/features/ingredientSearch/DrinksOutlet.test.jsx
@@ -2,15 +2,8 @@ import DrinksOutlet from "./DrinksOutlet";
 import { renderWithProviders } from "../../utils/test-utils";
 import { screen } from "@testing-library/react";
 
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  useParams: jest.fn(),
-}));
-
 describe("Tests for DrinksOutlet", () => {
   test("Should return 'Drinks by ingredients' heading", () => {
-    require("react-router-dom").useParams.mockReturnValue("");
-
     renderWithProviders(<DrinksOutlet />);
 
     const drinkHeading = screen.getByRole("heading", {
