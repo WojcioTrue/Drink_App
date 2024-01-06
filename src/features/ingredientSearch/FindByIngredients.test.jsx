ef11530ebb925332ba76ec4dfe86cc5eb5a597dd
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-01-06 15:28:54 +0100

    Wrote tests for FindByIngredients

---
+++ src/features/ingredientSearch/FindByIngredients.test.jsx
@@ -0,0 +1,21 @@
+import { screen } from "@testing-library/react";
+import React from "react";
+import "@testing-library/jest-dom";
+import { renderWithProviders } from "../../utils/test-utils";
+import FindByIngredients from "./FindByIngredients";
+import { setupStore } from "../../app/store";
+import { displayElement } from "./ingredientsButtonsSlice";
+
+
+
+describe("test for FindByIngredients compoenent", () => {
+  test("should return rendered component with  with 'find by ingredients' button", async () => {
+    const store = setupStore();
+    store.dispatch(displayElement());
+    renderWithProviders(<FindByIngredients/>);
+    const button = screen.getByRole("button", { name: /find by ingredients/i });
+    expect(button).toBeInTheDocument();
+
+    screen.debug();
+  });
+});
