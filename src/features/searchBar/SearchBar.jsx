9d5959363dfa2f14f5bcffa64dec1d1eeebfdf98
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-01-31 19:03:09 +0100

    Fixed reload in netlify deployment and deployment itself

--- src/features/searchBar/SearchBar.jsx
+++ src/features/searchBar/SearchBar.jsx
@@ -25,7 +25,7 @@ const Searchbar = () => {
             className="search-bar__logo"
             title="logo"
           />
-          FindMyDrink.X
+          FindMyDrink.
         </Link>
       </h2>
         <SearchBarInput/>
