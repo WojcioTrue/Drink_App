2621a1f0341a7bd4bbbbf442e699f1012fcb0eb7
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-02-29 17:07:28 +0100

    styling favouriteButton

--- src/sharedComponents/FavButton.tsx
+++ src/sharedComponents/FavButton.tsx
@@ -4,14 +4,11 @@ import { motion } from "framer-motion";
 import { mainView } from "../framerStyles/variants";
 // import FavListIcons from "../features/favouriteList/FavListIcons";
 import { useAppSelector } from "../app/storeHooks";
-import { useEffect } from "react";
 
 const FavButton = () => {
   const listOfFavourite = useAppSelector((state) => state.favouriteList)
 
-  useEffect(() => {
-    console.log(listOfFavourite)
-  }, [listOfFavourite])
+
   return (
     <Link to="/favourite_list">
       <motion.div
