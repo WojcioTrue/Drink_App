4650e42719d137730c96a20e71180bcd74c8beda
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-02-29 17:37:49 +0100

    Styled fav-list__icon and counter

--- src/sharedComponents/FavButton.tsx
+++ src/sharedComponents/FavButton.tsx
@@ -4,6 +4,8 @@ import { motion } from "framer-motion";
 import { mainView } from "../framerStyles/variants";
 // import FavListIcons from "../features/favouriteList/FavListIcons";
 import { useAppSelector } from "../app/storeHooks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 const FavButton = () => {
   const listOfFavourite = useAppSelector((state) => state.favouriteList)
@@ -18,8 +20,20 @@ const FavButton = () => {
         animate="show"
         className="fav-button"
       >
+        <motion.span
+              key="1"
+              initial={{ opacity: 0, x: -5 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 25 }}
+            >
+              <FontAwesomeIcon
+                className="fav-button__icon"
+                icon={faHeart}
+                beatFade
+              ></FontAwesomeIcon>
+            </motion.span>
+        <div className="fav-list__Counter">
         
-        <div className="favListCounter">
             <p>{listOfFavourite.length}</p>
         </div>
       </motion.div>
