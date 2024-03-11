45c7690558c7528617f3cfc328e7636ba3a41879
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-03-11 19:22:47 +0100

    Made 'DrinksByIngredient.test.tsx' code more readable

--- src/features/ingredientSearch/DrinksByIngredients.test.tsx
+++ src/features/ingredientSearch/DrinksByIngredients.test.tsx
@@ -25,22 +25,23 @@ const server = setupServer(
   )
 );
 
-beforeEach(() => {
-  const mockIntersectionObserver = jest.fn();
-  mockIntersectionObserver.mockReturnValue({
-    observe: () => null,
-    unobserve: () => null,
-    disconnect: () => null,
-  });
-  window.IntersectionObserver = mockIntersectionObserver;
-});
+// beforeEach(() => {
+//   const mockIntersectionObserver = jest.fn();
+//   mockIntersectionObserver.mockReturnValue({
+//     observe: () => null,
+//     unobserve: () => null,
+//     disconnect: () => null,
+//   });
+//   window.IntersectionObserver = mockIntersectionObserver;
+// });
 beforeAll(() => server.listen());
 afterAll(() => server.close());
 
-describe("test for full screen fetching drinks by ingredients", () => {
-  test("Should return no drinks by ingredient initially, no mocking implemented", () => {
+describe("Displaying 'no drinks by ingredient' screen initially", () => {
+  test("Should return 'There are no drinks with such ingredient's' header", () => {
+    // mocking required by component react-router-dom useParams hook with empty id
     require("react-router-dom").useParams.mockReturnValue({
-      id: "Gin&Light_rum",
+      id: "",
     });
     renderWithProviders(<DrinksByIngredients />);
 
@@ -49,17 +50,30 @@ describe("test for full screen fetching drinks by ingredients", () => {
     );
     expect(badIngredientsHeader).toHaveClass("header");
     expect(badIngredientsHeader).toBeInTheDocument();
+  });
+  test("Should return 'Change ingredients you chose.' message", () => {
+    require("react-router-dom").useParams.mockReturnValue({
+      id: "",
+    });
+    renderWithProviders(<DrinksByIngredients />);
 
-    const firstMessageLine = screen.getByText(/Change ingredients you chose./i);
-    expect(firstMessageLine).toBeInTheDocument();
-    const secondMessageLine = screen.getByText(
-      /Change ingredients you chose./i
-    );
-    expect(secondMessageLine).toBeInTheDocument();
+    const changeIngredientsMessage = screen.getByText(/Change ingredients you chose./i);
+    expect(changeIngredientsMessage).toBeInTheDocument();
   });
 
-  test("Mocking data for drinks by Ingredients. Ingredients are Gin and light rum", async () => {
-    // mock useLocation hook with Gin&Light_rum parameter
+  test("Should return 'Drink responsibly!' message", () => {
+    require("react-router-dom").useParams.mockReturnValue({
+      id: "",
+    });
+    renderWithProviders(<DrinksByIngredients />);
+
+    const drinkResponsibilyMessage = screen.getByText(/Drink responsibly!/i);
+    expect(drinkResponsibilyMessage).toBeInTheDocument();
+  });
+});
+
+describe("Mocking data for 'DrinksByIngredients'. 'Gin&Light_rum' as passed as ingredients", () => {
+  test("Should return '3-mile Long Island iced Tea", async () => {
     require("react-router-dom").useParams.mockReturnValue({
       id: "Gin&Light_rum",
     });
@@ -68,34 +82,65 @@ describe("test for full screen fetching drinks by ingredients", () => {
     // rendering first drink from mocked API
     const firstDrink = await screen.findByText("3-Mile Long Island Iced Tea");
     expect(firstDrink).toBeInTheDocument();
+  })
+  test("Should return element's with correct drink name", async () => {
+    require("react-router-dom").useParams.mockReturnValue({
+      id: "Gin&Light_rum",
+    });
+    renderWithProviders(<DrinksByIngredients />);
 
     for (let element of DrinksOutletData.drinks) {
-      // test element for correct drink name
-      const drinkName = screen.getByRole("heading", {
+      const drinkName = await screen.findByRole("heading", {
         name: element.strDrink,
         level: 4,
       });
       expect(drinkName).toBeInTheDocument();
-      // compare rendered img src with one from mocked data
-      const drinkImg = screen.getByAltText(element.strDrink);
+    }
+  })
+
+  test("Should check if element have valid 'src' path to img", async () => {
+    require("react-router-dom").useParams.mockReturnValue({
+      id: "Gin&Light_rum",
+    });
+    renderWithProviders(<DrinksByIngredients />);
+
+    for (let element of DrinksOutletData.drinks) {
+
+      const drinkImg = await screen.findByAltText(element.strDrink);
       expect(drinkImg).toHaveAttribute("src", element.strDrinkThumb);
     }
+  })
 
-    // compare links added by react-router to id passed from fetched data
-    const links = screen.getAllByRole("link");
+  test("Should return true if all drinks have correct links from mocked data", async () => {
+    require("react-router-dom").useParams.mockReturnValue({
+      id: "Gin&Light_rum",
+    });
+    renderWithProviders(<DrinksByIngredients />);
+
+    const links = await screen.findAllByRole("link");
     links.forEach((element, index) => {
       expect(element).toHaveAttribute(
         "href",
         `/drink/${DrinksOutletData.drinks[index].idDrink}`
       );
     });
+  })
 
-    // should render 4 buttons with 'details' text
-    const buttons = screen.getAllByRole("button", { name: /details/i });
+  test("should render 4 buttons with 'details' text", async () => {
+    require("react-router-dom").useParams.mockReturnValue({
+      id: "Gin&Light_rum",
+    });
+    renderWithProviders(<DrinksByIngredients />);
+
+    const buttons = await screen.findAllByRole("button", { name: /details/i });
     expect(buttons).toHaveLength(4);
-    
+
     //shold return 4 'addFavourite' buttons
     const favButtons = screen.getAllByTitle('addFavourite')
     expect(favButtons).toHaveLength(4);
-  });
-});
+  })
+})
+
+
+
+
