053e2a0cfc13a802b9e38c54ab2c3c0d104072d5
Wojciech.Otawski <wojciech.otawski@gmail.com>
2024-03-11 19:25:37 +0100

    Made 'DrinksOutlet.test.tsx' code more readable

--- src/features/ingredientSearch/DrinksOutlet.test.tsx
+++ src/features/ingredientSearch/DrinksOutlet.test.tsx
@@ -10,22 +10,43 @@ jest.mock("react-router-dom", () => ({
 }));
 
 describe("Tests for DrinksOutlet", () => {
-  test("Should return 'Something went wrong!' page", () => {
+  test("Should return 'something went wront' header", async () => {
     require("react-router-dom").useLocation.mockReturnValue({
       pathname: "/ingredients/",
     });
     renderWithProviders(<DrinksOutlet />);
 
-    const errorHeader = screen.getByText(/something went wrong!/i);
+    const errorHeader = await screen.findByText(/something went wrong!/i);
     expect(errorHeader).toBeInTheDocument();
-    const firstErrorMessage = screen.getByText(
+  });
+  test("Should return first paragraph of error message", async () => {
+    require("react-router-dom").useLocation.mockReturnValue({
+      pathname: "/ingredients/",
+    });
+    renderWithProviders(<DrinksOutlet />);
+
+    const firstErrorMessage = await screen.findByText(
       /i don't see the page you looking for, or some error occured.../i
     );
-    expect(firstErrorMessage).toBeInTheDocument();
-    const secondErrorMessage = screen.getByText(/return to home page./i);
-    expect(secondErrorMessage).toBeInTheDocument();
+    expect(firstErrorMessage).toBeInTheDocument()
+  })
+  test("Should return second paragraph of error message", async () => {
+    require("react-router-dom").useLocation.mockReturnValue({
+      pathname: "/ingredients/",
+    });
+    renderWithProviders(<DrinksOutlet />);
+    const secondErrorMessage = await screen.findByText(/return to home page./i);
+    expect(secondErrorMessage).toBeInTheDocument()
+  })
 
-    const returnButton = screen.getByRole("button", { name: /return home/i });
-    expect(returnButton).toBeInTheDocument();
-  });
+  test("Should return 'return home' button", async () => {
+    require("react-router-dom").useLocation.mockReturnValue({
+      pathname: "/ingredients/",
+    });
+    renderWithProviders(<DrinksOutlet />);
+    const returnButton = await screen.findByRole('button', { name: /return home/i })
+    expect(returnButton).toBeInTheDocument()
+  })
 });
+
+
